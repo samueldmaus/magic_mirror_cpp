68b2a8f49653593e9c42d74201c8a763569d33b6
Sam Maus <samueldmaus@gmail.com>
2021-01-01 20:31:55 -0600

    working on creating updating clock

--- src/TimeDate.cpp
+++ src/TimeDate.cpp
@@ -3,6 +3,11 @@
 MirrorFrame::MirrorFrame(const wxString& title)
        : wxFrame(NULL, wxID_ANY, title, wxPoint(-1, -1))
 {
-    wxTimer *timer = new wxTimer(this, -1);
-    timer->Start(200);
+
+    wxDateTime time_now = wxDateTime::Now();
+    int h = time_now.GetHour();
+    int m = time_now.GetMinute();
+    int s = time_now.GetSecond();
+    time.Printf("%i:%i:%i", h, m, s);
+
 }
--- src/TimeDate.h
+++ src/TimeDate.h
@@ -2,11 +2,13 @@
 #define TIME_DATE_H
 
 #include <wx/wx.h>
+#include <wx/dcbuffer.h>
 
 class MirrorFrame : public wxFrame
 {
 public:
     MirrorFrame(const wxString& title);
+    wxString time;
 };
 
 #endif
